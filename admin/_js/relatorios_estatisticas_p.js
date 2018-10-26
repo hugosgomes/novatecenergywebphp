cdfcbbe7ec76516575083e521d2c09c72433be9e
Claudio Henrique da Silva Martins <cmartins@novatecenergy.com.br>
2018-10-26 13:57:34 -0300

    Adição do gráfico de serviços em estatísticas GNS

--- admin/_js/relatorios_estatisticas_p.js
+++ admin/_js/relatorios_estatisticas_p.js
@@ -231,3 +231,57 @@ $(function() {
               ]
         });
     });
+
+ //GRÁFICO SERVIÇOS
+ var wc_chart = Highcharts.chart('jwc_chart_container_servicos', {
+            chart: {
+                type: 'line',
+                spacingBottom: 0,
+                spacingTop: 5,
+                spacingLeft: 0,
+                spacingRight: 20
+            },
+            title: {
+                text: 'Estatística Serviços'
+            },
+            subtitle: {
+                text: ''
+            },
+            yAxis: {
+                allowDecimals: false,
+                title: {
+                    text: 'Serviços Realizados'
+                }
+            },
+            tooltip: {
+                useHTML: true,
+                shadow: false,
+                headerFormat: '<p class="al_center" style="color:white">{point.key}</p><p class="al_center" style="font-size: 2em; color:white;">{point.y}</p>',
+                pointFormat: '<p class="al_center" style="color:white">{series.name}</p><p class="al_center"></p>',
+                backgroundColor: 'rgba(87, 125, 216, 0.60)',
+                borderWidth: 0,
+                padding: 20,
+                style: {
+                    padding: 20,
+                    color: '#fff'
+                }
+            },
+            xAxis: {
+                categories: ['01/2018','02/2018','03/2018','04/2018','05/2018','06/2018','07/2018','08/2018','09/2018','10/2018','11/2018','12/2018'],
+                minTickInterval: 1
+            },
+            series: [
+                {
+                    name: 'Serviços Realizados',
+                    data: [20,20,25,30,28,25,28,20,20,22,30,35],
+                    color: '#259A77',
+                    lineColor: '#259A77'
+                },
+                {
+                    name: 'Serviços Faturados',
+                    data: [15,15,20,20,25,22,25,15,18,20,30,30],
+                    color: '#0E96E5',
+                    lineColor: '#0E96E5'
+                },
+            ]
+        });
